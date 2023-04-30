ffee2890da7a85d2109df8afc8ecc56195d3fb68
lucagm94 <109768295+lucagm94@users.noreply.github.com>
2023-04-30 16:18:42 -0300

    Resolucion de problema que no actualizaba git
    El problema estaba en que movi los archivos de carpeta, asi que no reconocia los cambios.

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -7,17 +7,20 @@ const ACCELERATION: int = 250;
 const DECELERATION: int = 390;
 var canDobleJump:bool = false;
 var direction:int;
+var estado: String;
 
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 func _physics_process(delta):
-	Jump(delta)
-	Motion(delta)
+	jump(delta)
+	motion(delta)
+	$Feedback.set_text(str(canDobleJump))
+	animations()
 	move_and_slide()
 
-func Jump(delta):
+func jump(delta):
 	
 	if Input.is_action_just_pressed("ui_accept"):
 		jumpVelocity = -400
@@ -26,29 +29,32 @@ func Jump(delta):
 		jumpVelocity = -600
 		
 	if not is_on_floor():
-		$Spritesheet.play("run")
 		velocity.y += gravity * delta
+		estado = "jump"
 
 	if is_on_floor():
 		canDobleJump  = true
+		estado = "idle"
 		
 	if Input.is_action_just_pressed("ui_accept") or Input.is_action_just_pressed("largeJump") and is_on_floor():
-		velocity.y = jumpVelocity
+		velocity.y = jumpVelocity 
 		
-	if Input.is_action_just_pressed("ui_accept") or Input.is_action_just_pressed("largeJump") and  !is_on_floor() and canDobleJump:
+	if Input.is_action_just_pressed("ui_accept") and canDobleJump:
 		velocity.y = jumpVelocity
 		canDobleJump = false
-		
-func Motion(delta):
+	
+	if Input.is_action_just_pressed("largeJump") and canDobleJump:
+		velocity.y = jumpVelocity
+		canDobleJump = false
+
+func motion(delta):
 	
 	if Input.is_action_pressed("incSpeed"):
-		maxSpeed = 600;
+		maxSpeed = 1000;
 		
 	if Input.is_action_just_released("incSpeed"):
 		maxSpeed = 400
-
-	$Feedback.set_text(str(velocity.x))
-	
+		
 	if Input.is_action_pressed("ui_left"):
 		direction = -1
 		
@@ -68,25 +74,35 @@ func Motion(delta):
 			
 	if Input.is_action_just_released("ui_right") or Input.is_action_just_released("ui_left"):
 		direction = 0;
+		if estado != "jump":
+			estado = "idle"
 		
 	if direction == 0:
-		$Spritesheet.play("Idle")
 		if velocity.x < 0:
 			velocity.x += DECELERATION*delta;
 		if velocity.x > 0:
 			velocity.x -= DECELERATION*delta;
+
+func animations():
 	
 	if direction == 1: 
-
 		$Spritesheet.flip_h = false;
-		if is_on_floor():
-			$Spritesheet.play("run")
+		if estado != "jump":
+			estado = "run"
+			
+	if direction == -1:
+		$Spritesheet.flip_h = true
+		if estado != "jump":
+			estado = "run"
 		
-		if direction == -1:
-			$Spritesheet.flip_h = true
-			if is_on_floor():
-				$Spritesheet.play("run")
+	if estado == "idle":
+		$Spritesheet.play("idle")
 
+	if estado == "run" && estado != "jump":
+		$Spritesheet.play("run")
+				
+	if estado == "jump":
+		$Spritesheet.play("jump")
 
 
 
